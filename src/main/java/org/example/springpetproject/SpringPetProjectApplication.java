084a9b8f9968ed5ef78bfa141695b3127cef7e5d
ivan.omelchenk <ivan.omelchenko@paydock.com>
2024-05-13 17:54:12 +0100

    git init

---
+++ src/main/java/org/example/springpetproject/SpringPetProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.springpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springpetproject/SpringPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springpetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
